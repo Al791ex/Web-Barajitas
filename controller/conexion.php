c4a8f8045d05cc857821fb2741b0bbb955124734
LuichoX <101019471+LuichoX@users.noreply.github.com>
2024-07-25 04:06:17 -0400

    las 4 paginas

--- controller/conexion.php
+++ controller/conexion.php
@@ -1,8 +1,18 @@
 <?php
+
     $db_host = "sumas-restas.mysql.database.azure.com";
     $db_user = "al791ex08";
     $db_pass = "Richard%";
     $db_name = "sumas_db";
+
+
+
+/*
+    $db_host = "localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "sumas_db";
+*/
 
     $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
