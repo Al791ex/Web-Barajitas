6cd8ffabd899ac8a914a789d0d2c0648a86bcce9
Alexander <71038444+Al791ex@users.noreply.github.com>
2024-07-25 19:40:42 -0400

    Update index.php

--- index.php
+++ index.php
@@ -10,8 +10,8 @@ include("controller/login_user.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="login.css">
-    <!-- Boxicons -->
-    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+    <!-- Boxicons 
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script> -->
 
     <!-- Bootstrap -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
